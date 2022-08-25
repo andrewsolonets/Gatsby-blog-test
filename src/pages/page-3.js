eec2c0f00a181db29aceaf0ab7e06b8c84a6c0ea
Andrew Solonets <andrewsolonets@gmail.com>
2022-08-25 21:21:23 +0100

    first

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hello from the third page</h1>
+  </Layout>
+)
+
+export default ThirdPage
